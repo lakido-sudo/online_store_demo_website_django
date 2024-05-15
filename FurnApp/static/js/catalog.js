d28113f41d9fe6f10a933775f9d215456d26a374
lakido <lakido551@gmail.com>
2024-05-15 14:01:01 +0300

    some changes catalog

--- FurnApp/static/js/catalog.js
+++ FurnApp/static/js/catalog.js
@@ -2,18 +2,17 @@ document.addEventListener('DOMContentLoaded', function() {
     const catalogButton = document.querySelector('.catalog-button');
     const catalogContainer = document.getElementById('catalog');
     const categoriesDiv = document.querySelector('.categories');
-    const detailsDiv = document.querySelector('.details');
+    const subcategoriesDiv = document.querySelector('.subcategories');
 
     catalogButton.addEventListener('click', function() {
         if (catalogContainer.style.display === 'none') {
             catalogContainer.style.display = 'flex';
-            detailsDiv.style.display = 'flex'; // Показываем блок подкатегорий при открытии каталога
             // Загрузка данных категорий после разворачивания каталога
             fetch('/categories/')
                 .then(response => response.json())
                 .then(data => {
                     categoriesDiv.innerHTML = ''; // Очистка предыдущих категорий
-                    detailsDiv.innerHTML = ''; // Очистка предыдущих подкатегорий
+                    subcategoriesDiv.innerHTML = ''; // Очистка предыдущих подкатегорий
 
                     data.forEach(category => {
                         const categoryElement = document.createElement('div');
@@ -21,31 +20,31 @@ document.addEventListener('DOMContentLoaded', function() {
                         categoryElement.textContent = category.name;
 
                         categoryElement.addEventListener('click', function() {
+                            // Удалить класс selected со всех блоков категорий
+                            const categoryBlocks = document.querySelectorAll('.category-block');
+                            categoryBlocks.forEach(block => block.classList.remove('selected'));
+
+                            // Добавить класс selected к выбранному блоку категории
+                            this.classList.add('selected');
+
+                            // Загрузить подкатегории для выбранной категории
                             const subcategories = category.subcategories;
-                            const subcategoriesDiv = document.createElement('div');
-                            subcategoriesDiv.classList.add('subcategories');
-
-                            subcategories.forEach(subcategory => {
-                                const subcategoryElement = document.createElement('div');
-                                subcategoryElement.classList.add('subcategory-block');
-                                subcategoryElement.textContent = subcategory.name;
-                                subcategoriesDiv.appendChild(subcategoryElement);
-                            });
-
-                            detailsDiv.innerHTML = `
+                            subcategoriesDiv.innerHTML = `
                                 <h2>${category.name}</h2>
-                                <p>${category.description}</p>
-                                <h3>Подкатегории:</h3>
+                                <div class="subcategory-blocks">
+                                    ${subcategories.map(subcategory => `
+                                        <div class="subcategory-block">${subcategory.name}</div>
+                                    `).join('')}
+                                </div>
                             `;
-                            detailsDiv.appendChild(subcategoriesDiv);
                         });
+
 
                         categoriesDiv.appendChild(categoryElement);
                     });
                 });
         } else {
             catalogContainer.style.display = 'none';
-            detailsDiv.style.display = 'none'; // Скрываем блок подкатегорий при закрытии каталога
         }
     });
 });
