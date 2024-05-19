3e63ac5b2758dde8218d36bb25fcb079c81ec8a2
lakido <lakido551@gmail.com>
2024-05-19 14:36:28 +0300

    home to index names

--- FurnApp/urls.py
+++ FurnApp/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.index, name='index'),  # Изменили 'home' на 'index'
     path('categories/', views.category_list, name='category_list'),
     path('categories/<slug:slug>/', views.category_detail, name='category_detail'),
     path('product/<slug:category_slug>/<slug:product_slug>/', views.product_detail, name='product_detail'),
--- FurnApp/views.py
+++ FurnApp/views.py
@@ -5,8 +5,8 @@ from .models import Category, Product
 from django.shortcuts import render
 
 
-def home(request):
-    return render(request, 'home.html')
+def index(request):
+    return render(request, 'index.html')
 
 
 def category_list(request):
