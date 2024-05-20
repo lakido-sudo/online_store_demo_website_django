6772353e9c50ab79ada11079aeb0f61fe043ffa0
lakido <lakido551@gmail.com>
2024-05-20 19:58:20 +0300

    settings 1

--- Furnstore/settings.py
+++ Furnstore/settings.py
@@ -110,7 +110,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/staticfiles/'
 STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR)
