ae8bcd4eb7e231c7fc001b0362c070a938087fcd
lakido <lakido551@gmail.com>
2024-05-20 20:02:13 +0300

    settings 2

--- Furnstore/settings.py
+++ Furnstore/settings.py
@@ -111,9 +111,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/staticfiles/'
-STATICFILES_DIRS = [BASE_DIR / "staticfiles"]
+STATICFILES_DIRS = [BASE_DIR]
 #STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
-STATIC_ROOT = os.path.join(BASE_DIR)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Default primary key field type
